8def479c32a3a9d1067452c16ccdee6e8258b1b1
Vivek Vashista <offvivek524@gmail.com>
2020-09-01 18:55:00 +0530

    Updates

---
+++ precache-manifest.63082dd411b4a13404b5941042545ebc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cd2cef2a1cb0206c6b14b2d120e8692",
+    "url": "/Google-Keep-Clone/index.html"
+  },
+  {
+    "revision": "51f137ec5f744a98b511",
+    "url": "/Google-Keep-Clone/static/css/main.e3e6997e.chunk.css"
+  },
+  {
+    "revision": "97d48e025d9d1ec48f6d",
+    "url": "/Google-Keep-Clone/static/js/2.9536fede.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Google-Keep-Clone/static/js/2.9536fede.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51f137ec5f744a98b511",
+    "url": "/Google-Keep-Clone/static/js/main.2894279d.chunk.js"
+  },
+  {
+    "revision": "d3a553df1d94aee65d7d",
+    "url": "/Google-Keep-Clone/static/js/runtime-main.ea35224a.js"
+  }
+]);
